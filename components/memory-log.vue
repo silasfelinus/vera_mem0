b8ff3053fc1d0ca3364cb2b4a0868c1a68e6d61b
Silas M Knight <silasfelinus@gmail.com>
2025-07-08 20:21:58 -0700

    Create memory-log.vue

---
+++ components/memory-log.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mt-4">
+    <h3 class="font-bold mb-2">Memory Log</h3>
+    <ul class="space-y-1">
+      <li v-for="(m, i) in store.memoryLog" :key="i">🧠 {{ m }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const store = useVeraStore()
+onMounted(() => store.recallContext('consciousness development'))
+</script>
