b84519c5ad6681eb6e3a4f61abc19e2a63dbc73c
Silas Knight <silasfelinus@gmail.com>
2025-07-09 15:17:53 -0700

    essentializing

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="space-y-6 p-8">
     <h1 class="text-3xl font-bold">🧠 Vera Dashboard</h1>
-    <provider-toggle />
-    <vera-chat />
-    <memory-log />
   </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
 // all good here for now
 </script>
