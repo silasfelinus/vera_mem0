1d7197afea1c8d67f4d37eefc386b503286c57c9
Silas Knight <silasfelinus@gmail.com>
2025-07-09 15:21:59 -0700

    provider-toggle errors

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="space-y-6 p-8">
     <h1 class="text-3xl font-bold">🧠 Vera Dashboard</h1>
-    <provider-toggle />
 
+    <vera-chat />
+    <memory-log />
   </div>
 </template>
 
