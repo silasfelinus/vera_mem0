51a8027477b63728f139f407b834872f5e360763
Silas Knight <silasfelinus@gmail.com>
2025-07-09 15:20:12 -0700

    re-adding components one at a time

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="space-y-6 p-8">
     <h1 class="text-3xl font-bold">🧠 Vera Dashboard</h1>
+    <provider-toggle />
+
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang='ts'>
 // all good here for now
 </script>
