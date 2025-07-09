96c433ec1c3e97f1b2949ae0d0d66a3499afbc2f
Silas Knight <silasfelinus@gmail.com>
2025-07-08 21:46:13 -0700

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,26 @@
-// @ts-check
+// eslint.config.js
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    // Allow self-closing void elements
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/html-self-closing': 'off',
+    // other rules you want to customize
+  },
+  ignores: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    '.nuxt/',
+    '.output',
+    '.env',
+    'cypress/*',
+    'spec*',
+    'cypress/',
+    'cypress/e2e/',
+    'sample/',
+    // add other patterns you want to ignore
+  ],
+})
