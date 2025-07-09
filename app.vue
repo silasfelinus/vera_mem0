382583dbb5959c718b60cc59dff5c28bfc7bf650
Silas Knight <silasfelinus@gmail.com>
2025-07-09 15:38:06 -0700

    testing no store

--- app.vue
+++ app.vue
@@ -4,13 +4,3 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { onMounted } from 'vue'
-import { useVeraStore } from '@/stores/veraStore'
-
-const veraStore = useVeraStore()
-
-onMounted(() => {
-  veraStore.initializeConversation()
-})
-</script>
